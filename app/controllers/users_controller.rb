6efee7dab025e6532f5cf43b96a7ce266201c4b9
koh-suzuki <k.s_coward_0629@icloud.com>
2019-09-12 06:20:42 +0000

    signup-user Finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,10 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
-      
+      flash[:success] = "新規作成に成功しました。"
+      redirect_to @user
     else
       render :new
     end
@@ -15,4 +16,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
 end
