faa39d1b9dcfadd2d541adf6ea35de10d93b9351
koh-suzuki <k.s_coward_0629@icloud.com>
2019-09-13 21:30:53 +0000

    login-function Finish

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,11 +6,16 @@ module SessionsHelper
   
   def current_user
     if session[:user_id]
-      if @current_user
-        @current_user = User.find_by(id: params[:user_id])
-      else
-        @current_user
-      end
+      @current_user ||= User.find_by(id: session[:user_id])
     end
+  end
+  
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
+  def logged_in?
+    !current_user.nil?
   end
 end
