36c8d9513f85985b23f2588c163b9fb5c5e55b60
koh-suzuki <k.s_coward_0629@icloud.com>
2019-09-13 12:37:57 +0000

    6章6.3.2途中

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  def current_user
+    if session[:user_id]
+      if @current_user
+        @current_user = User.find_by(id: params[:user_id])
+      else
+        @current_user
+      end
+    end
+  end
 end
